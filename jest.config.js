e2586d3a590c922f33fb2ae06e243b4599d9d1fc
joaosoutto <joaosoutto@hotmail.com>
2022-01-10 17:31:32 -0300

    installing RTL and making first test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/'],
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
 };
---
+++ src/components/MainTest/index.tsx
@@ -0,0 +1,7 @@
+const MainTest = () => (
+  <main>
+    <h1>Testing example</h1>
+  </main>
+);
+
+export default MainTest;
