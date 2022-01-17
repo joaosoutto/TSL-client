c314cf55fbf85b2ccc9ad8f5f0a05172f64a35d2
joaosoutto <joaosoutto@hotmail.com>
2022-01-17 12:49:07 -0300

    Pages: Sign-in and Sign-up - redirecting home with token

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,12 +1,29 @@
+import { ReactElement } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useEffect } from 'react';
+
+import getToken from '../../services/getToken';
+
 import FormCover from '../../components/FormCover';
 import FormSignUp from '../../components/FormSignUp';
 import * as S from './styles';
 
-const SignUp = () => (
-  <S.Wrapper>
-    <FormCover />
-    <FormSignUp />
-  </S.Wrapper>
-);
+const SignUp = (): ReactElement => {
+  const history = useHistory();
+  useEffect(() => {
+    const userToken = getToken();
+
+    if (userToken) {
+      history.push('/');
+    }
+  }, [history]);
+
+  return (
+    <S.Wrapper>
+      <FormCover />
+      <FormSignUp />
+    </S.Wrapper>
+  );
+};
 
 export default SignUp;
--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,12 +1,29 @@
+import { ReactElement } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useEffect } from 'react';
+
+import getToken from '../../services/getToken';
+
 import FormCover from '../../components/FormCover';
 import FormSignIn from '../../components/FormSignIn';
 import * as S from './styles';
 
-const SignIn = () => (
-  <S.Wrapper>
-    <FormCover />
-    <FormSignIn />
-  </S.Wrapper>
-);
+const SignIn = (): ReactElement => {
+  const history = useHistory();
+  useEffect(() => {
+    const userToken = getToken();
+
+    if (userToken) {
+      history.push('/');
+    }
+  }, [history]);
+
+  return (
+    <S.Wrapper>
+      <FormCover />
+      <FormSignIn />
+    </S.Wrapper>
+  );
+};
 
 export default SignIn;
